949aa83ff3037d58b6e37e0147f04eb241234835
Frank Doyle <gh@macdoyle.com>
2021-04-06 20:30:28 -0400

    changing endpoints and auth

--- js/config.js
+++ js/config.js
@@ -1,13 +1,13 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_TopaRydIw', // e.g. us-east-2_uXboG5pAb
-        samUserPoolId: 'us-east-1_S0an1RGWz',
-        samUserPoolClientId: '42v3ahumie57uh4k8pv7hhofpd',
+        samUserPoolId: 'us-east-1_k4io4XcVp',
+        samUserPoolClientId: '7pkabte8jit45ak4b4aa977rih',
         userPoolClientId: '7iii0er2g5vace6huibgp41h99', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://0oz8198zg6.execute-api.us-east-1.amazonaws.com/prod',
-        invokeUrlSam: 'https://nyzsbc59sf.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrlSam: 'https://8mrtlex681.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
